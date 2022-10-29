a25791e4793c7ae4b7d2b5e72849efab41c570ce
mkmark <mark@mkmark.net>
2022-10-29 12:56:39 +0800

    fix shader color

--- src/components/webgl/shaders/node.full.frag.glsl
+++ src/components/webgl/shaders/node.full.frag.glsl
@@ -10,17 +10,15 @@ const float radius_2 = 0.5;
 
 void main(void) {
   float radius_1 = radius_2 - v_ring1_width;
-  vec4 v_color_light = v_color * 1.2;
-  v_color_light[3] = v_color[3];
   vec4 v_color_dark = v_color * 0.8;
   v_color_dark[3] = v_color[3];
   float distToCenter = length(gl_PointCoord - vec2(0.5, 0.5));
 
   float t = 0.0;
   if (distToCenter < radius_1 - v_border)
-    gl_FragColor = v_color_light;
+    gl_FragColor = v_color;
   else if (distToCenter < radius_1)
-    gl_FragColor = mix(v_color_dark, v_color_light, (radius_1 - distToCenter) / v_border);
+    gl_FragColor = mix(v_color_dark, v_color, (radius_1 - distToCenter) / v_border);
   else if (distToCenter < radius_2 - v_border)
     gl_FragColor = v_color_dark;
   else if (distToCenter < radius_2)
